35e45dd2030e372b7c9f140f9949c128c00b1d6b
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2025-09-12 09:44:41 +0530

    Simon Says game

--- Mini-projects/JavaScript/Simon Says Game/script.js
+++ Mini-projects/JavaScript/Simon Says Game/script.js
@@ -49,28 +49,66 @@ function userFlash(btn){
 }
 
 function levelUp() {
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor((Math.random() * 3) + 1);
+    let randIdx = Math.floor((Math.random() * 4));
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randBtn);
+
+    // console.log(randIdx);
+    // console.log(randColor);
+    // console.log(randBtn);
+
+    gameSeq.push(randColor);
+    console.log(gameSeq)
     // flash on choosing random button
-    Flash(randBtn);
+    gameFlash(randBtn);
 }
 
 // Button Event Listners
 
+function checkAns(idx) {
+    // console.log("Current Level" , level);
+
+    if(userSeq[idx] == gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp , 1000);
+        }
+    } else {
+        h2.innerHTML = `Game Over! Your Score was <b>${level}</b> <br> Press Any Key to Start`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function () {
+            document.querySelector("body").style.backgroundColor = "white";
+        },150)
+        resetGame();
+    }
+
+
+}
+
 function btnPress(){
     let btn = this;
+    // console.log(btn);
     userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    console.log(`User Entered ${userColor}`);
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll('.btn');
 
 for (btn of allBtns){
-    btn.addEventListener("click" , btnPress);
+    btn.addEventListener("click" , btnPress)
+};
+
+function resetGame() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
